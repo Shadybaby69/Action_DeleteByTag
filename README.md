302a37de7a505f640065d4d42568118fe95bff8d
jerome von Daak <jerome.vondaak@proton.me>
2025-03-16 14:50:28 +0100

    initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Action_DeleteOnTag
-Action that deletes all assets of a release, speceified by tag
+Action that deletes all assets of a release, specified by tag
---
+++ index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
+const fs = require('fs');
+
+(async () => {
+    try {
+        // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+        const github = new GitHub(process.env.GITHUB_TOKEN)
+
+        // Get owner and repo from context of payload that triggered the action
+        const { owner: currentOwner, repo: currentRepo } = context.repo
+
+        // Owner of Repo and Repo itself to get Tag from, as input or from context
+        const assetOwner = core.getInput('owner', { required: false }) || currentOwner
+        const assetRepo = core.getInput('repo', { required: false }) || currentRepo
+        const assetTag = core.getInput('asset_tag', { required: false } || 'Latest')
+
+        // Getting the uploadUrl of the Release with the Latest tag
+        const releaseIdResponse = await github.repos.getReleaseByTag({
+            owner: assetOwner,
+            repo: assetRepo,
+            tag: assetTag
+        })
+        // The releaseId with specified tag
+        const releaseId = releaseIdResponse.data
+        core.info(`Id of the Repo with tag: ${assetTag} is ${releaseId}`)
+
+        //Getting all release assets
+        const releaseAssetsResponse = await github.repos.listReleaseAssets({
+            owner: assetOwner,
+            repo: assetRepo,
+            release_id: releaseId
+        })
+
+        // Deleting all Assets from specified tag
+        const releaseAsset = releaseAssetsResponse.data
+        releaseAsset.forEach((assetId) => {
+            github.repos
+                .deleteReleaseAsset({
+                    owner: assetOwner,
+                    repo: assetRepo,
+                    asset_id: assetId
+                })
+                .then()
+            core.info(`- Deleting asset with id: ${assetId}`)
+        })
+
+        core.info("The action was successfully executed!");
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+})();
